1f1b193185f0bd3b93c9c4e353ab29a202a64537
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2021-12-05 15:57:04 +0100

    Configure whitelist

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"log/syslog"
 	"time"
@@ -24,9 +25,25 @@ func main() {
 		return
 	}
 
+	botPoller := &tb.LongPoller{Timeout: 15 * time.Second}
+
+	senderWhiteList := tb.NewMiddlewarePoller(botPoller, func(upd *tb.Update) bool {
+		if upd.Message == nil {
+			return true
+		}
+
+		sender_id := upd.Message.Sender.ID
+		if _, allowed_sender := botConfig.TelegramBot.AllowedSenders[sender_id]; !allowed_sender {
+			log.Println("Blocked message received from sender", sender_id, ".")
+			return false
+		}
+
+		return true
+	})
+
 	bot, err := tb.NewBot(tb.Settings{
 		Token:  botConfig.TelegramBot.Token,
-		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+		Poller: senderWhiteList,
 	})
 
 	if err != nil {
@@ -36,8 +53,11 @@ func main() {
 
 	bot.Handle("/hello", func(m *tb.Message) {
 		sender_id := m.Sender.ID
-		log.Println("/hello received from sender", sender_id, ".")
-		bot.Send(m.Sender, "Hello World!")
+		sender_name := botConfig.TelegramBot.AllowedSenders[sender_id].Name
+		log_msg := fmt.Sprintf("/hello received from sender %s.", sender_name)
+		log.Println(log_msg)
+		response := fmt.Sprintf("Hello %s.", sender_name)
+		bot.Send(m.Sender, response)
 	})
 
 	bot.Start()
